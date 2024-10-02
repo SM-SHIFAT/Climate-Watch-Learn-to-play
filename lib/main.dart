ab699cad98364ccbade0b458e842a2e1663cf6d1
Eagle-x Online <mahfuzrahman129@gmail.com>
2024-10-03 00:50:18 +0600

    Orientation set to landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Set landscape orientation
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Climate Watch: Play to learn, Learn to play",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
